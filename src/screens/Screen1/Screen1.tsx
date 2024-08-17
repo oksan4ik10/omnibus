2aac734f11d1471b0f81e7d61dd7dc488283b27a
Kalinina Oksana <ksan4ik92@gmail.com>
2024-08-17 17:15:34 +0300

    feat: add lamp

--- src/screens/Screen1/Screen1.tsx
+++ src/screens/Screen1/Screen1.tsx
@@ -3,9 +3,36 @@ import style from "./Screen1.module.scss"
 function Screen1() {
     return (
         <div className={style.container}>
+            <div className={style.logo}>
+                <img src="./images/screen1/logo.png" alt="logo" />
+            </div>
+            <div className={style.lamp}>
+                <img src="./images/lamp.svg" alt="lamp" />
+            </div>
+            <main className={style.main}>
+                <div className={style.main__people}>
+                    <div className={style.main__img}>
+                        <img src="./images/screen1/people.png" alt="people" />
+                    </div>
+                </div>
+                <div className={style.main__info + " " + style.info}>
+                    <div className={style.info__img}>
+                        <img src="./images/screen1/label.png" alt="people" />
+                    </div>
+                    <div className={style.info__line}>
+                        <img src="./images/screen1/square.svg" alt="square" />
+                        <div></div>
+                    </div>
+                    <div className={style.info__text}>
+                        <p>Все люди делятся на два типа — на тех, кто работают по&nbsp;специальности, и тех, кто выбирает совсем другую сферу после учебы. А какой путь выбираешь ты?</p>
+                        <p>Твоя карьера может складываться интересно вне зависимости от&nbsp;твоего выбора! Мы собрали реальные карьерные истории сотрудников Х компаний и на их примере показали, что ограничения только у нас в голове. У тебя есть много вариантов работы по&nbsp;специальности, а если хочется что-то изменить, то помни&nbsp;— большинство компаний смотрят не на строчку «Образование» в&nbsp;резюме, а на твои знания, интересы и навыки.</p>
+                    </div>
+                    <button className={style.info__btn + " btn"}>Дальше</button>
+                </div>
+            </main>
 
         </div>
-    );
+    )
 }
 
 export default Screen1;
