91e231ae2d2fedf93864622cf36e2ddc727c384f
Kalinina Oksana <ksan4ik92@gmail.com>
2024-08-19 08:44:33 +0300

    feat: add card with style

--- src/screens/Screen2/Screen2.tsx
+++ src/screens/Screen2/Screen2.tsx
@@ -19,6 +19,31 @@ function Screen2() {
                 <span>подарки</span>
                 <span>подарки</span>
             </div>
+            <div className={style.cards}>
+                <div className={style.card}>
+                    <div className={style.card__photo}>
+                        <img src="/images/cards/1.png" alt="test" />
+
+                    </div>
+                    <div className={style.card__info + " " + style.info}>
+                        <div className={style.info__title}>
+                            <span className={style.info__name}>Ксюша</span>
+                            <span className={style.info__age}>23 года</span>
+                        </div>
+                        <div className={style.info__job}>
+                            <span>FutureToday </span>
+                            <span>Старший продюссер спецпроектов</span>
+                        </div>
+                        <div className={style.info__text}>
+                            <span>Дима отвечает за коммуникацию с клиентами, за их удержание и развитие отношений с ними. Он — связующее звено между клиентами и компанией.</span>
+                        </div>
+                        <div className={style.water}>
+                            <img src="images/cards/water.svg" alt="water" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
         </div>
     );
