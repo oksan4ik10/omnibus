fda56ae6bc8460c3b6f4c0f298afb9a8dde6043c
Kalinina Oksana <ksan4ik92@gmail.com>
2024-08-20 09:05:25 +0300

    feat: add adaptive screen3

--- src/screens/Screen3/Screen3.tsx
+++ src/screens/Screen3/Screen3.tsx
@@ -1,8 +1,14 @@
 import Presents from "../../components/Presents/Presents";
 
+import style from "./Screen3.module.scss"
+
 function Screen3() {
     return (
-        <Presents isMobile={true}></Presents>
+        <div className={style.container}>
+            <Presents isMobile={true}></Presents>
+            <button className={"btn " + style.btn}>ИГРАТЬ</button>
+        </div>
+
     );
 }
 
