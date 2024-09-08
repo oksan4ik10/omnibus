dc752c2907c9e4db171fcdbff2d842c3035952df
Kalinina Oksana <ksan4ik92@gmail.com>
2024-09-08 18:44:51 +0300

    fix: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,25 +23,114 @@ function Footer(props: IProps) {
                 speed={5000}
                 freeMode={true}
                 loop={true}
+                loopAdditionalSlides={2}
                 autoplay={{
                     delay: 0,
                     disableOnInteraction: false,
                 }}
-                // modules={[Autoplay]}
+                modules={[Autoplay]}
                 className={style.mySwiper + " mySwiper"}
             >
-                <SwiperSlide><img src="/images/footer/1.png" alt="1" /></SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide1 + " " + style.slide}>
+                        <img src="/images/footer/1.png" alt="1" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide2 + " " + style.slide}>
+                        <img src="/images/footer/2.png" alt="1" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide3 + " " + style.slide}>
+                        <img src="/images/footer/3.png" alt="13" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide4 + " " + style.slide}>
+                        <img src="/images/footer/4.png" alt="4" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide5 + " " + style.slide}>
+                        <img src="/images/footer/5.png" alt="5" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide6 + " " + style.slide}>
+                        <img src="/images/footer/6.png" alt="6" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide7 + " " + style.slide}>
+                        <img src="/images/footer/7.png" alt="7" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide8 + " " + style.slide}>
+                        <img src="/images/footer/8.png" alt="8" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide9 + " " + style.slide}>
+                        <img src="/images/footer/9.png" alt="9" />
+                    </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                    <div className={style.slide1 + " " + style.slide}>
+                        <img src="/images/footer/1.png" alt="1" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide2 + " " + style.slide}>
+                        <img src="/images/footer/2.png" alt="1" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide3 + " " + style.slide}>
+                        <img src="/images/footer/3.png" alt="13" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide4 + " " + style.slide}>
+                        <img src="/images/footer/4.png" alt="4" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide5 + " " + style.slide}>
+                        <img src="/images/footer/5.png" alt="5" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide6 + " " + style.slide}>
+                        <img src="/images/footer/6.png" alt="6" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide7 + " " + style.slide}>
+                        <img src="/images/footer/7.png" alt="7" />
+                    </div>
+                    </SwiperSlide>
+                <SwiperSlide>
+                <div className={style.slide8 + " " + style.slide}>
+                        <img src="/images/footer/8.png" alt="8" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className={style.slide9 + " " + style.slide}>
+                        <img src="/images/footer/9.png" alt="9" />
+                    </div>
+                    </SwiperSlide>
+
+                {/* <SwiperSlide><img src="/images/footer/1.png" alt="1" /></SwiperSlide>
                 <SwiperSlide><img src="/images/footer/2.png" alt="2" /></SwiperSlide>
                 <SwiperSlide><img src="/images/footer/3.png" alt="3" /></SwiperSlide>
                 <SwiperSlide><img src="/images/footer/4.png" alt="4" /></SwiperSlide>
                 <SwiperSlide><img src="/images/footer/5.png" alt="5" /></SwiperSlide>
                 <SwiperSlide><img src="/images/footer/6.png" alt="6" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/1.png" alt="1" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/2.png" alt="2" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/3.png" alt="3" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/4.png" alt="4" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/5.png" alt="5" /></SwiperSlide>
-                <SwiperSlide><img src="/images/footer/6.png" alt="6" /></SwiperSlide>
+                <SwiperSlide><img src="/images/footer/7.png" alt="7" /></SwiperSlide>
+                <SwiperSlide><img src="/images/footer/8.png" alt="8" /></SwiperSlide>
+                <SwiperSlide><img src="/images/footer/9.png" alt="9" /></SwiperSlide> */}
             </Swiper>
 
 
