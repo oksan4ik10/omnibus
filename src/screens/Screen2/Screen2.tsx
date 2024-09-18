e5383111dd5802cec8c56312e8ccf077854105f7
Kalinina Oksana <ksan4ik92@gmail.com>
2024-09-18 12:31:18 +0300

    fix: screen2

--- src/screens/Screen2/Screen2.tsx
+++ src/screens/Screen2/Screen2.tsx
@@ -15,15 +15,15 @@ function Screen2(props: IProps) {
 
 
     const cardInfo=    {
-        "name": "Данил",
-        "age": "23 года",
+        "name": "Дима",
+        "age": "28 лет",
         "work": "FutureToday",
         "workDesc": "Тим-лид аккаунт-менеджеров",
-        "cardDesc": "Я аккаунт-менеджер, и моя работа позволяет мне трудиться из любой точки мира — у меня удаленка. Я обсуждаю с&nbsp;клиентами лучшие практики на&nbsp;рынке и&nbsp;наших питомцев, попивая оранж джус с&nbsp;видом на&nbsp;Эльбрус. После работы мы с&nbsp;коллегами можем сходить в бар, что тоже приятно. Конечно, есть и&nbsp;пара минусов: я постоянно на&nbsp;встречах, поэтому не&nbsp;успеваю играть со&nbsp;своим собакеном каждые пять минут, и почему‑то не&nbsp;все клиенты дарят мне подарки на&nbsp;Новый год. Но я их все равно люблю ❤️",
+        "cardDesc": "Я — руководитель команды аккаунтов FutureToday. Выполняю роль амбассадора наших продуктов в&nbsp;мире HR и Employer Branding: каждый день встречаюсь с&nbsp;клиентами, обсуждаю их потребности и подбираю лучшие решения для их задач. Особенно мне нравится, что я всегда в курсе новинок и могу влиять на&nbsp;то, что происходит в&nbsp;бизнесе. У меня полная удалёнка, поэтому могу работать из&nbsp;любой точки мира, попивая джус с&nbsp;видом на&nbsp;Эльбрус&nbsp;:)",
         "answerRight": "left",
         "indexPhoto": 9,
-        "colorBtn": "#EA5616",
-        "colorJob": "#EA5616",
+        "colorBtn": "#F46D00",
+        "colorJob": "##F46D00",
         "bgPhoto": "#F46D00",
         "answerText": "<p>Данил учился на&nbsp;менеджера в&nbsp;СПБГЭУ. Ещё в&nbsp;старшей школе видел себя бизнесменом — стремился понять, как работает коммерция, какие существуют бизнес-процессы. В&nbsp;вузе зря времени не&nbsp;терял — участвовал в&nbsp;кейс-чемпионатах, а&nbsp;после выпуска активно искал работу. На&nbsp;дне карьеры узнал о&nbsp;программе «Звезды Балтики», пообщался с&nbsp;эйчаром, попал на&nbsp;первый собес и школьная мечта сбылась! На&nbsp;работе Данил чувствует себя предпринимателем, только управляет не&nbsp;компаний, а&nbsp;отдельным брендом Балтики.</p><p>Свою профессию Данил ценит за&nbsp;динамику и возможность влиять на продукт. А ещё любит коллег, офис со&nbsp;спортзалом, уютной столовой и атмосферу драйва, когда вокруг постоянный штурм идей.</p><p>Чтобы быть предпринимателем не&nbsp;обязательно открывать своё ООО, приходи в&nbsp;Балтику и отвечай за&nbsp;бренды, знакомые каждому)</p>",
         "answerRectColorSuccess": "#08A652",
@@ -61,8 +61,16 @@ function Screen2(props: IProps) {
             <div className={style.lamp}>
                         <img src="./images/lamp.svg" alt="lamp" />
                     </div>
+                    <div className={style.lampNew}>
+                        <img src="./images/sceen2/lamp-new.svg" alt="lamp" />
+                    </div>
             <div className={style.card}>
-
+            <div className={style.card__text}>
+                                          {!isMobile &&<div className={style.waterPresentsMan}>
+                        <img src="./images/sceen2/water-btn.png" alt="water" />
+                    </div>}
+                    Сможешь угадать, кто из этих людей работает по специальности, а кто — нет? Среди участников, прошедших все карточки, мы разыграем призы!
+                </div>
                 <div className={style.card__item}>
 
                     <div className={style.man}>
@@ -76,18 +84,16 @@ function Screen2(props: IProps) {
                             <div className={style.calcImg}>
                                 <img src="images/sceen2/calc.svg" alt="calc" />
                             </div>
-                            {!isMobile &&<div className={style.waterPresentsMan}>
+                            {/* {!isMobile &&<div className={style.waterPresentsMan}>
                         <img src="./images/sceen2/water-btn.png" alt="water" />
-                    </div>}
+                    </div>} */}
                     </div>
 
                     {isMobile && <div className={style.waterPresents}>
                         <img src="./images/sceen2/water-btn.png" alt="water" />
                     </div>}
                 </div>
-                <div className={style.card__text}>
-                    Сможешь угадать, кто из этих людей работает по специальности, а кто — нет? Среди участников, прошедших все карточки, мы разыграем призы!
-                </div>
+
 
 
             </div>
