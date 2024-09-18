1e92c9c4b72cf4e6d58aaac0f819af966329f44a
Kalinina Oksana <ksan4ik92@gmail.com>
2024-09-18 17:26:35 +0300

    feat: add validation form

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -1,8 +1,15 @@
 
+import { FTClient } from 'ft-client'
 import style from "./Forms.module.scss"
+import { SubmitHandler, useForm } from 'react-hook-form';
 interface IProps{
     closeForm: ()=> void
     openFinish: ()=> void
+}
+interface IForm{
+    name: string;
+    email: string;
+    checkbox: boolean;
 }
 function Forms(props: IProps) {
     const {closeForm, openFinish} = props;
@@ -12,14 +19,34 @@ function Forms(props: IProps) {
         closeForm()
         
     }
-    const sendForm = (e: React.MouseEvent<HTMLButtonElement>)=> {
-        e.preventDefault()
+
+    const { register, handleSubmit, formState: { errors }} = useForm<IForm>({
+        defaultValues: {
+          name: '',
+          email: '',
+          checkbox: false,
+        }
+      });
+
+    const sendForm: SubmitHandler<IForm> = async  (dataForm: IForm)=> {
+        console.log(dataForm);
+        
+        const ftClient = new FTClient(
+            'https://ft-admin-api.sjuksin.ru/',
+            'noregrets'
+          )
+          const projectState = await ftClient.loadProjectState()
+          console.log('Project state', projectState)
+        const data = { post: 'user@mail.ru', name: "test"}
+        // const data = { post: 'user@mail.ru', name: "test"}
+        const newRecord = await ftClient.createRecord(data)
+        console.log('new record:', newRecord)
         openFinish()
     }
 
     return (
         <div className={style.wrapper} onClick={(e)=>clickCloseForm(e)}>
-            <form className={style.form}>
+            <form className={style.form} onSubmit={handleSubmit(sendForm)}>
             <div className={style.close} onClick={closeForm}>
                 <img src="/images/cards/close.svg" alt="close" />
             </div>
@@ -28,18 +55,19 @@ function Forms(props: IProps) {
                     <p className={style.form__text}>Мы разыгрываем карьерные консультации, сертификаты на&nbsp;маркетплейсы и&nbsp;подписки на&nbsp;сервисы! Оставляй почту и имя, чтобы участвовать. Результаты объявим <strong>19&nbsp;ноября.</strong></p>
                     <div className={style.form__items}>
                         <label className={style.form__label}>
-                            <input type="email" placeholder="Введите почту" />
+                            <input className={(errors.email ? style.error : "")} type="email" placeholder="Введите почту" {...register("email", { required: true, pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Некорректный email"}})} />
                         </label>
                         <label className={style.form__label}>
-                            <input type="text" placeholder="Введите ваше имя" />
+                            <input className={(errors.email ? style.error : "")} type="text" placeholder="Введите ваше имя" {...register("name", { required: true})} />
                         </label>
-                        <label className={style.form__checkbox}>
-                            <input type="checkbox" />
+                        <label className={style.form__checkbox + " " + (errors.checkbox ? style.errorCheck : "")}>
+                            <input type="checkbox" {...register("checkbox", { required: true})}/>
                             <span>Я согласен(а) на <a href="https://fut.ru/personal-data" target="_blank" className={style.link}>обработку персональных данных</a> и получение информационных сообщений, а также с&nbsp;<a href="https://noregrets.fut.ru/agreement.pdf" className={style.link} target="_blank">правилами проведения акции</a></span>
                         </label>
                     </div>
 
-                    <button className={style.form__btn + " btn"} onClick={(e)=>sendForm(e)}>Отправить</button>
+                    <button className={style.form__btn + " btn"}>Отправить</button>
                 </div>
 
             </form>
